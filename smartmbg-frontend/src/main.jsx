a9842070a2b1632808b0cba1b85fe863f883e12a
Fian <fian.kurus06@gmail.com>
2026-06-28 17:12:57 +0700

    Add global fetch interceptor to inject ngrok skip browser warning header

--- smartmbg-frontend/src/main.jsx
+++ smartmbg-frontend/src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Global fetch override to inject ngrok-skip-browser-warning header
+const originalFetch = window.fetch;
+window.fetch = async function () {
+  let [resource, config] = arguments;
+  
+  if (typeof resource === 'string' && resource.includes('ngrok-free.app')) {
+    config = config || {};
+    config.headers = config.headers || {};
+    config.headers['ngrok-skip-browser-warning'] = '69420';
+    arguments[1] = config;
+  }
+  
+  return originalFetch.apply(this, arguments);
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
